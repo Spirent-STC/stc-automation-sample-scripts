9d4682b2012236b48deb5159998473a3035ad23e
Vijay Chennepalli <vijay.chennepalli@spirent.com>
2025-07-17 13:34:44 +0530

    updated aion license_server sample script

--- python/Aion_License_Server_Login_Logout_sample.py
+++ python/Aion_License_Server_Login_Logout_sample.py
@@ -1,6 +1,6 @@
 # File Name:                 Aion_Licnese_Server_Login_Logout_sample.py
 # Description:               This script demonstrates login Aion License Server,
-#                            reserve stc ports and sign-out after completion of test suites 
+#                            Reserve stc ports, Verify chassis license and sign-out after completion of test suites 
 
 # This loads the TestCenter library.
 from StcPython import StcPython
@@ -13,12 +13,12 @@ print("SpirentTestCenter system version:\t", stc.get("system1", "version"))
 
 #Sign in AION License Server
 print("Signing AION License Server")
-stc.perform("TemevaSignInCommand", Server=<server_ip>, Username=<user_name>, Password=<password>)
-stc.perform("TemevaSetWorkspaceCommand", Workspace=<workspacename>)
+stc.perform("TemevaSignInCommand", Server="<server_ip>", Username="<user_name>", Password="<password>")
+stc.perform("TemevaSetWorkspaceCommand", Workspace="<workspace>")
 
 #Alternative way to sign in AION License Server
 print("Signing AION License Server using a refresh token")
-stc.perform("TemevaSignInWithTokenCommand", Server=<server_ip>, RefreshToken=<refreshtoken>)
+stc.perform("TemevaSignInWithTokenCommand", Server="<server_ip>", RefreshToken="<refreshtoken>")
 
 # Physical topology
 szChassisIp1 = "10.29.0.49"
@@ -35,9 +35,6 @@ print("Creating ports ...")
 hPortTx = stc.create("port", under=hProject, location=txPortLoc, useDefaultHost=False)
 hPortRx = stc.create("port", under=hProject, location=rxPortLoc, useDefaultHost=False)
 
-# Configure physical interface.
-hPortTxCopperInterface = stc.create("EthernetCopper",  under=hPortTx)
-
 # Attach ports. 
 # Connects to chassis, reserves ports and sets up port mappings all in one step.
 # By default, connects to all previously created ports.
@@ -47,6 +44,10 @@ stc.perform("AttachPorts")
 # Apply the configuration.
 print("Apply configuration")
 stc.apply()
+chassis1 = stc.perform("ChassisConnectCommand", Hostname=szChassisIp1)
+chassis2 = stc.perform("ChassisConnectCommand", Hostname=szChassisIp2)
+
+print(stc.perform("LicenseCheckCommand", ChassisList=[chassis1['OutputChassisList'], chassis2['OutputChassisList']]))
 
 # Disconnect from chassis, release ports, and reset configuration.
 print("Release ports and disconnect from chassis")
@@ -57,7 +58,7 @@ print("Deleting project")
 stc.delete(hProject)
 
 #Sign out Aion License Server
-#Note: It is recommended to sign out of the server after completing the test suites 
+#Note: It is recommended to sign out of the license server after completing the test suites
 print("Sign out Aion license server")
 stc.perform("TemevaSignOutCommand")
 
